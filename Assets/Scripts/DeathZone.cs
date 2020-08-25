7706185639948e5c42931416c2e91c47d99c661c
KickAps <flo.berson06@gmail.com>
2020-08-25 15:15:09 +0200

    ep 10-11 / transition niveau suivant & respawn

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathZone : MonoBehaviour
+{
+    private Transform playerSpawn;
+    private Transform mainCamera;
+
+    private Animator fadeSystem;
+
+    private void Awake()
+    {
+        playerSpawn = GameObject.FindGameObjectWithTag("PlayerSpawn").transform;
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").transform;
+        fadeSystem = GameObject.FindGameObjectWithTag("FadeSystem").GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            StartCoroutine(respawnPlayer(collision));
+        }
+    }
+
+    public IEnumerator respawnPlayer(Collider2D collision)
+    {
+        fadeSystem.SetTrigger("FadeIn");
+        yield return new WaitForSeconds(1f);
+        collision.transform.position = playerSpawn.position;
+        mainCamera.position = playerSpawn.position;
+
+        PlayerHealth playerHealth = collision.transform.GetComponent<PlayerHealth>();
+        playerHealth.TakeDamage(30);
+    }
+}
